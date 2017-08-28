0174e8a25e51be561646d2d6ee4a41f04ee9e29d
jvazquez96 <jorgevzqz6@gmail.com>
2017-08-28 15:57:34 -0500

    Add tokens

--- scanner.py
+++ scanner.py
@@ -3,3 +3,18 @@
 # Finito Determinista como una Matríz de Transiciones
 
 import sys
+
+# tokens
+
+VAR  = 101 # Variable
+CTE  = 102 # Constante
+SEPA = 103 # Separador
+LRP  = 104 # Parentesis izquierdo
+RRP  = 105 # PArentesis derecho
+COMA = 106 # Coma
+PUNT = 107 # Punto
+PUNI = 108 # Predicado unario
+BUNI = 109 # Predicado binario
+CTF  = 110 # Cuantificador
+TIL  = 111 # Tilde ~ 
+ERR  = 200 # Error
