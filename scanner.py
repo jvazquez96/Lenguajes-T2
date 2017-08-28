b5747dad0a71b6afaa017403d6c6cc6f60b71144
jvazquez96 <jorgevzqz6@gmail.com>
2017-08-28 18:23:00 -0500

    Filled matrix but we need a lot of changes on it

--- scanner.py
+++ scanner.py
@@ -18,3 +18,14 @@ BUNI = 109 # Predicado binario
 CTF  = 110 # Cuantificador
 TIL  = 111 # Tilde ~ 
 ERR  = 200 # Error
+
+# Matriz de transiciones: codificacion del AFD
+# [renglon, columna] = [estado no final, transicion]
+#      a-z  A-Z    0-9         _       @CUB  @TET  @DOD  @IZQ  @DER  @ADE   @DET    @A   @E    ~        &    |      ->     <->     $      (      )     ,     .   \t " "
+MT = [[  1,   2, PENDIENTE, PENDIENTE,    3,    3,    3,    4,    4,    4,     4,   5,   5,   TIL,   SEPA, SEPA,  SEPA,   SEPA,  SEPA,  LRP,   RRP,  COM, PUNT,   END],
+	  [1, ERR, 1, 1, VAR, VAR, VAR, VAR, VAR, VAR, VAR, VAR, VAR, TIL, VAR, VAR, VAR , VAR, VAR, VAR, VAR, VAR, VAR, VAR],
+	  [ERR, 2, 2, 2, CTE, CTE, CTE, CTE, CTE, CTE, CTE, CTE, CTE, TIL, CTE, CTE, CTE, CTE, CTE, CTE, CTE, CTE, CTE, CTE],
+	  [PUNI, PUNI, PUNI, PUNI, 3, 3, 3, PUNI, PUNI, PUNI, PUNI, PUNI, PUNI, TIL, PUNI, PUNI, PUNI, PUNI, PUNI, PUNI, PUNI, PUNI, PUNI, PUNI],
+	  [BUNI, BUNI, BUNI, BUNI, BUNI, BUNI, BUNI, 4, 4, 4, 4, BUNI, BUNI, TIL, BUNI, BUNI, BUNI, BUNI, BUNI, BUNI, BUNI, BUNI],
+	  [CTF, CTF, CTF, CTF, CTF, CTF, CTF, CTF, CTF, CTF, CTF, 5, 5, TIL, CTF, CTF, CTF, CTF, CTF, CTF, CTF, CTF, CTF, CTF], 
+	  [ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR< ERR< ERR, ERR, ERR< ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR, ERR]]
